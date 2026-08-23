514c6557b0332aa39631f853828d0520505d1d46
Muhammad Fauzi Maulana <fauzimaulana.id@gmail.com>
2026-08-23 17:31:41 +0700

    chore(deps): add retrofit, okhttp, ksp, hilt and paging

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -7,23 +9,39 @@ android {
     compileSdk {
         version = release(37)
     }
-
+    buildFeatures {
+        buildConfig = true
+    }
     defaultConfig {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "BASE_URL", "\"https://api.themoviedb.org/3/\"")
+        buildConfigField("String", "API_TOKEN", "\"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTAwZTBiYzM3MmY5Y2FjNjEzYTNhNmQxNmQwNGQ2YyIsIm5iZiI6MTc4NzQwNjQ0Mi42MzkwMDAyLCJzdWIiOiI2YTg5YTg2YTU0N2YzMzkzMzlhMTBkODciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.FPs40No44DJ0skuW9PrttZC0ccfOKMoovF7dmCm-tBY\"")
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    //Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    //Paging
+    implementation(libs.paging.common)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
