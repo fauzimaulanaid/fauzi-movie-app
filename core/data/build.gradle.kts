96aea2ece8ec2eb833b2631b4fe61ee6338a35c0
Muhammad Fauzi Maulana <fauzimaulana.id@gmail.com>
2026-08-23 21:44:58 +0700

    chore(deps): setup dependencies for core:data

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -24,6 +26,18 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation(projects.core.model)
+    implementation(projects.core.network)
+    implementation(projects.core.domain)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    //Paging
+    implementation(libs.paging.common)
+    implementation(libs.paging.runtime)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
