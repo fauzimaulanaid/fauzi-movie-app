4927a0647bd9367257b6c5ac963cdf37cf767e1d
Muhammad Fauzi Maulana <fauzimaulana.id@gmail.com>
2026-08-23 20:32:34 +0700

    feat(model): add movie and review data model

---
+++ core/model/src/main/java/com/fauzimaulana/fauzimovieapp/core/model/data/MovieModel.kt
@@ -0,0 +1,10 @@
+package com.fauzimaulana.fauzimovieapp.core.model.data
+
+data class MovieModel(
+    val id: Int = 0,
+    val posterPath: String = "",
+    val backdropPath: String = "",
+    val title: String = "",
+    val releaseDate: String = "",
+    val overview: String = "",
+)
