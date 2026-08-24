f356b5a96f9da91c238f413fb8384c168f667e46
Muhammad Fauzi Maulana <fauzimaulana.id@gmail.com>
2026-08-24 15:32:40 +0700

    feat(movie): load reviews based on movie id

--- feature/movie/src/main/java/com/fauzimaulana/fauzimovieapp/feature/movie/detail/DetailViewModel.kt
+++ feature/movie/src/main/java/com/fauzimaulana/fauzimovieapp/feature/movie/detail/DetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.fauzimaulana.fauzimovieapp.feature.movie.detail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
@@ -12,8 +13,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    getMovieReviewsUseCase: GetMovieReviewsUseCase
+    getMovieReviewsUseCase: GetMovieReviewsUseCase,
+    savedStateHandle: SavedStateHandle
 ): ViewModel() {
+
+    private val movieId: Int = checkNotNull(savedStateHandle["movie_id"])
+
     val movieReviews: Flow<PagingData<ReviewModel>> =
-        getMovieReviewsUseCase(969681).cachedIn(viewModelScope)
+        getMovieReviewsUseCase(movieId).cachedIn(viewModelScope)
 }
