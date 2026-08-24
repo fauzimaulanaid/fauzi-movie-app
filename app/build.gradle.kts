799de1abfbfeea356993b0230ed527fd4aaf4672
Muhammad Fauzi Maulana <fauzimaulana.id@gmail.com>
2026-08-24 09:02:53 +0700

    refactor: move ui theme related resources to core:ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
+    implementation(projects.core.ui)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
