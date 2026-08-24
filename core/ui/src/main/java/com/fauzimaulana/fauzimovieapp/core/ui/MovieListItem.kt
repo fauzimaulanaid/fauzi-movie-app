b6a1deb9a1edd766d2f162a5153a5e1b1103ab8d
Muhammad Fauzi Maulana <fauzimaulana.id@gmail.com>
2026-08-24 11:12:43 +0700

    feat(ui): add reusable list item components

---
+++ core/ui/src/main/java/com/fauzimaulana/fauzimovieapp/core/ui/MovieListItem.kt
@@ -0,0 +1,74 @@
+package com.fauzimaulana.fauzimovieapp.core.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.fauzimaulana.fauzimovieapp.core.model.data.MovieModel
+import com.fauzimaulana.fauzimovieapp.core.ui.theme.FauziMovieAppTheme
+
+@Composable
+fun MovieListItem(
+    movie: MovieModel,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .width(150.dp)
+    ) {
+        AsyncImage(
+            model = "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+            contentDescription = movie.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(150.dp)
+                .height(200.dp)
+                .clip(RoundedCornerShape(8.dp)),
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = movie.title,
+            style = MaterialTheme.typography.bodyMedium.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            text = movie.releaseDate,
+            style = MaterialTheme.typography.bodySmall,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MovieListItemPreview() {
+    FauziMovieAppTheme {
+        MovieListItem(
+            movie = MovieModel(
+                id = 1,
+                title = "Spider-Man: No Way Home",
+                releaseDate = "2025-12-15",
+                overview = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                posterPath = "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+                backdropPath = "https://image.tmdb.org/t/p/original/7iwUUcKURMT7aKfCwMy6YnGtchD.jpg"
+            )
+        )
+    }
+}
